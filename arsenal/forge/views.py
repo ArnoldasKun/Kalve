c19eeb50b1118a554a500cd38c47b5984d8793c0
Arnoldas Kundrotas <arnokund@gmail.com>
2022-12-06 14:48:44 +0200

    static, 1st templates, base, index

--- arsenal/forge/views.py
+++ arsenal/forge/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . models import Blacksmith, Armor, ArmorType, ArmorOrder
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Welcome to blacksmiths!")
+    armor_count = Armor.objects.count()
+    armor_order_count = ArmorOrder.objects.count()
+    blacksmith_count = Blacksmith.objects.count()
+
+    context = {
+        'book_count': armor_count,
+        'book_instance_count': armor_order_count,
+        'blacksmith_count': blacksmith_count,
+        'genre_count': ArmorType.objects.count()
+    }
+
+    return render(request, 'library/index.html', context)
