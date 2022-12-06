368e032041cf46d827fb571a25e7743826813fae
Arnoldas Kundrotas <arnokund@gmail.com>
2022-12-06 20:13:29 +0200

    bs view, armors listview, views, urls, html

--- arsenal/forge/urls.py
+++ arsenal/forge/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('blacksmiths/', views.blacksmiths, name='blacksmiths'),
+    path('blacksmith/<int:blacksmith_id>/', views.blacksmith, name='blacksmith'),
+    path('armors/', views.ArmorListView.as_view(), name='armors'),
 ]
