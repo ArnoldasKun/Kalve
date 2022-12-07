a9d8c5b799606b6ba6bce43c739353d1994354a6
Arnoldas Kundrotas <arnokund@gmail.com>
2022-12-07 14:23:00 +0200

    + visists count

--- arsenal/forge/views.py
+++ arsenal/forge/views.py
@@ -10,12 +10,15 @@ def index(request):
     armor_count = Armor.objects.count()
     armor_order_count = ArmorOrder.objects.count()
     blacksmith_count = Blacksmith.objects.count()
+    visits_count = request.session.get('visits_count', 1)
+    request.session['visits_count'] = visits_count + 1
 
     context = {
         'armor_count': armor_count,
         'armor_order_count': armor_order_count,
         'blacksmith_count': blacksmith_count,
-        'armor_type_count': ArmorType.objects.count()
+        'armor_type_count': ArmorType.objects.count(),
+        'visits_count': visits_count,
     }
 
     return render(request, 'forge/index.html', context)
