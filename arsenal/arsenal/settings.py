f29ba5fad1040d9767ba1afce31df1a7430c051c
Arnoldas Kundrotas <arnokund@gmail.com>
2022-12-07 14:42:23 +0200

    user profile + login, settings, urls, css, html

--- arsenal/arsenal/settings.py
+++ arsenal/arsenal/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'forge',
+    'user_profile',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ MEDIA_ROOT = BASE_DIR.joinpath('media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
