94d50439d465334ab643adc57082df31d9dd3083
Arnoldas Kundrotas <arnokund@gmail.com>
2022-12-08 16:32:30 +0200

    fixed mistakes

--- arsenal/arsenal/settings.py
+++ arsenal/arsenal/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user_profile',
+    'forge',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'forge',
-    'user_profile',
     'tinymce',
 ]
 
